4bdde422d81f8f8b6e777e86d07dd48888704768
tkieungan-thk <t_kieungan@thk-hd.vn>
2025-11-05 08:26:53 +0700

    feat: 01 - auth  - đăng nhập  - đăng ký  - đăng xuất  - cập nhật mk  - quên mk

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <title>@yield('title', 'Authentication') | Stexo Admin</title>
+    <meta content="Responsive admin theme build on top of Bootstrap 4" name="description" />
+    <meta content="Themesdesign" name="author" />
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
+
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/css/metismenu.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div class="accountbg"></div>
+    <div class="home-btn d-none d-sm-block">
+        <a href="{{ url('/') }}" class="text-white"><i class="fas fa-home h2"></i></a>
+    </div>
+
+    <div class="wrapper-page">
+        <div class="card card-pages shadow-none">
+            <div class="card-body">
+                <div class="text-center m-t-0 m-b-15">
+                    <a href="{{ url('/') }}" class="logo logo-admin">
+                        <img src="{{ asset('assets/images/logo-dark.png') }}" alt="" height="24">
+                    </a>
+                </div>
+
+                <h5 class="font-18 text-center">@yield('page-title')</h5>
+
+                @yield('content')
+
+            </div>
+        </div>
+    </div>
+
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/metismenu.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.slimscroll.js') }}"></script>
+    <script src="{{ asset('assets/js/waves.min.js') }}"></script>
+    <script src="{{ asset('assets/js/app.js') }}"></script>
+</body>
+
+</html>
